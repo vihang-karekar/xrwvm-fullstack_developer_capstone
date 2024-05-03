148a2617f336b8be67d4842f81964172c9feb95a
vihang-karekar <72259561+vihang-karekar@users.noreply.github.com>
2024-05-03 12:27:48 +0530

    Update models.py - fixing spaces

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -26,10 +26,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
-    ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
 
     def __str__(self):
         return self.name  # Return the name as the string representation
