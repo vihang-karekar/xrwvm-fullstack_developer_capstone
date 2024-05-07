af66db3c2b633c39ab645f5080b48d5833008d31
Vihang Karekar <vihang.karekar@gmail.com>
2024-05-07 06:31:36 -0400

    Lint fixes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
          TemplateView.as_view(template_name="index.html")),
     path('postreview/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
-    path('searchcars/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('searchcars/<int:dealer_id>',
+         TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
